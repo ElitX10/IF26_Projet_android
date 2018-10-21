5dfcd005397dee198325c7a28e89d432afff3584
ElitX10 <thomas.le_scolan@sfr.fr>
2018-10-21 19:11:25 +0200

    feat : permissions géolocalisation
    Ajout de la permission pour utiliser la géolocalisation de l'appareil. Ajout d'un écran indiquant que la géolocalisation est obligatoire pour utiliser l'application.

---
+++ app/src/main/java/fr/utt/if26/if26_projet_android/Constant.java
@@ -0,0 +1,9 @@
+package fr.utt.if26.if26_projet_android;
+
+/**
+ * Classe contenat les constantes du projet
+ */
+public class Constant {
+    public static final int MY_ACCESS_FINE_LOCATION = 1;
+    public static final String request_geo_loc_text = "Vous devez activer la géolocalisation pour pouvoir utiliser l'application.";
+}
--- app/src/main/java/fr/utt/if26/if26_projet_android/MapsActivity.java
+++ app/src/main/java/fr/utt/if26/if26_projet_android/MapsActivity.java
@@ -1,7 +1,20 @@
 package fr.utt.if26.if26_projet_android;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +23,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
+    private ImageView image_pika;
+    private Button show_map_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +38,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        image_pika = findViewById(R.id.map_Image);
+        show_map_button = findViewById(R.id.show_map_button);
+
+        //check permisions :
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+//              Deuxieme connection et suivantes :
+                showSnabar();
+            }else {
+//              Premiere demande de droirs
+                askMapPermission();
+            }
+        }else {
+            image_pika.setVisibility(View.INVISIBLE);
+            show_map_button.setVisibility(View.INVISIBLE);
+        }
     }
 
+
+    /**
+     * affiche le popup pour accepter la localisation
+     */
+    private void askMapPermission(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                Constant.MY_ACCESS_FINE_LOCATION);
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +82,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            this.recreate();
+        }else{
+            showSnabar();
+        }
+    }
+
+    /**
+     * Affiche la snackbar pour autorisé la glocalisation
+     */
+    private void showSnabar(){
+        Snackbar.make(findViewById(R.id.map),
+                Constant.request_geo_loc_text,
+                Snackbar.LENGTH_LONG).setAction("Activer", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askMapPermission();
+            }
+        }).show();
     }
 }
